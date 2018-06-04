38dd77288c51aad0cdfb130db9c6fdc3f82a5661
victorsantana <35177247+victorsantana01@users.noreply.github.com>
2018-06-03 23:10:03 -0300

    Update ExemploGit.java
    1.2

--- src/exemplogit/ExemploGit.java
+++ src/exemplogit/ExemploGit.java
@@ -15,7 +15,7 @@ public class ExemploGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Teste Git");
+        System.out.println("Teste GitHub");
     }
     
 }
